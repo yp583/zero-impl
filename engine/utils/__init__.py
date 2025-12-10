894bb3472709c683822288cd119ad26e68a39b2e
yp583 <pately583@gmail.com>
2025-12-09 19:17:09 -0800

    remove unused import of reduce_scatter_uneven from engine.utils.distributed

--- engine/utils/__init__.py
+++ engine/utils/__init__.py
@@ -1,4 +1,4 @@
-from engine.utils.distributed import rank0_print, rank_print, all_gather_uneven, reduce_scatter_uneven
+from engine.utils.distributed import rank0_print, rank_print, all_gather_uneven
 from engine.utils.module_helpers import has_direct_params, get_module_tree, get_shard_numels
 from engine.utils.visualization import hierarchy_pos, graph_module
 from engine.utils.math import get_tensor_bytes, overlap
