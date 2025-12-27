cad56266e3c15e9d2798078406c877d213e5d05f
yp583 <pately583@gmail.com>
2025-12-27 15:39:58 -0500

    refactor class structure to use Protocols for parameter definitions and remove unused superclass logic

--- test.py
+++ test.py
@@ -1,33 +1,10 @@
-from torch.optim import Optimizer
+from typing import TypeAlias
+import torch
+from typing import Protocol
 
-class SuperClass:
-    def __init_subclass__(cls) -> None:
-        print(cls.__name__)
+class ParamProto(Protocol)
+    param: torch.nn.Parameter
+class ShardedFields(torch.nn.Parameter, Protocol):
+    shard_id: int
 
-class SubClassBefore(SuperClass):
-    def __init__(self):
-        print("hi before")
-
-def new_init(self):
-    print("hi from super")
-
-@classmethod
-def new_init_subclass(cls):
-    cls.__init__ = new_init
-
-def set_all_subclass_inits(cls):
-    for scls in cls.__subclasses__():
-        scls.__init__ = new_init
-
-SuperClass.__init_subclass__ = new_init_subclass
-set_all_subclass_inits(SuperClass)
-
-
-class SubClassAfter(SuperClass):
-    def __init__(self):
-        print("hi after")
-    
-
-SubClassBefore()
-SubClassAfter()
 
